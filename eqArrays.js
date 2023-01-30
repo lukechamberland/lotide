b3bc9290daa38a8f15b299a17f54f45a6788b7df
Luke Chamberland <luke.chamberland@shaw.ca>
2023-01-30 07:26:08 +0000

    more changes

--- eqArrays.js
+++ eqArrays.js
@@ -23,4 +23,7 @@ const eqArrays = function(first, second) {
   return true
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1],), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
