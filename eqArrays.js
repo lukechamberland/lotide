53fbb2670fd12e324cd0317912bee2a3c0ffc4e5
Luke Chamberland <luke.chamberland@shaw.ca>
2023-01-30 01:12:32 +0000

    Pushing to GitHub

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
+      return false;
+    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
+      eqArrays(first[i], second[i]);
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+    }
+    return true
+  }
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))
