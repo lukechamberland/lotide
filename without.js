fed6505ca3df3e38bc14fe474dad12c580a7202f
Luke Chamberland <luke.chamberland@shaw.ca>
2023-01-30 21:02:33 +0000

    pushing to GitHub

---
+++ without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
+      return false;
+    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
+      eqArrays(first[i], second[i]);
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqArrays2 = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const without = function(items, itemsToRemove) {
+  const array = []
+    for (const item of items) {
+      if (!itemsToRemove.includes(item)) {
+        array.push(item)
+      }
+    }
+    return array
+  }
