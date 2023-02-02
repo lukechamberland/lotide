ac4ce39c660753929ac02c61f804b7d000d8b3b3
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-02 02:43:31 +0000

    Pushing to GithUb

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
+      return false;
+    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
+      eqArrays(first[i], second[i]);
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqArrays2 = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertArraysEqual(['hello'], ['hello!']);
+
+const letterPositions = function(finalSentance) {
+  let newSentance = finalSentance.split('');
+  let results = {};
+  newSentance.forEach(function(letter, i) {
+    if (letter !== " ") {
+      results[letter] ? results[letter].push(i) : results[letter] = [i];
+    }
+  });
+  return results;
+};
