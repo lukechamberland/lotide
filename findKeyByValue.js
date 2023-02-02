e69c1d7fb2d4747af5d831f99635cee2a43371f0
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-02 06:24:14 +0000

    Changes

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
 const findKeyByValue = function(object, value) {
   for (let key in object) {
     if (value === object[key]) {
