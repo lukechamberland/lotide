08e48e61a89985ea5198a5ef4f18e1aa395c3dfb
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-02 00:26:05 +0000

    pushing to GitHub

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const countLetters = function(string) {
+  let newString = string.split(" ").join('');
+  let result = {};
+  for (let element of newString) {
+    result[element] ? result[element] += 1 : result[element] = 1;
+  }
+  return result;
+};
+
+console.log(countLetters('hello'))
