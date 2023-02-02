6c2577354b6d4c54c0c7708b79adce9330abc388
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-02 06:59:33 +0000

    new file to github

---
+++ eqObjects.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const eqArrays = require("./eqArrays");
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if ((typeof(object1[key]) !== typeof({}) && typeof(object2[key]) !== typeof({})) && (object1[key] !== object2[key])) {
+      return false;
+    } else if ((typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) && eqObjects(object1[key], object2[key]) === false) {
+      return false;
+    } else if (typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) {
+      eqObjects(object1[key], object2[key]);
+    }
+  }
+  return true;
+};
