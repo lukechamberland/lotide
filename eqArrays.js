12a29b48ca5c742247480950720dea7ffd8eef5d
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 07:05:06 +0000

    Pushing to GitHub

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+const assertEqual = require('./assertEqual')
 
 
 const eqArrays = function(first, second) {
@@ -22,8 +16,4 @@ const eqArrays = function(first, second) {
   }
   return true
 }
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1],), false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+module.exports = eqArrays
