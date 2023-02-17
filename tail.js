b202906e0d5c67a900775217eee48eb53920eda2
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 22:39:01 +0000

    Pushing changes with require

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
 const tail = function(thisArray) {
-  return thisArray.slice(0);
+  return thisArray.slice(1);
 }
-assertEqual(tail(['heyo', 33, 'yes', true], [33, 'yes', true]))
+
+module.exports = tail;
