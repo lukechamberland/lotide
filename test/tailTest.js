c625e2caa8ef23cba83d0cecacf7057963c54e7a
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 22:27:21 +0000

    Pushing changes with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,4 +6,7 @@ describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
     assertArraysEqual(tail([1, 2, 3]), [2, 3]);
   });
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assertArraysEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
 });
--- test/testMiddle.js
+++ test/testMiddle.js
@@ -5,4 +5,7 @@ describe("#middle", () => {
   it("returns 2 for [1, 2, 3]", () => {
     assertArraysEqual(middle([1, 2, 3]), 2);
   });
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
 });
