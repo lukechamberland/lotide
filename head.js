7c4692a3ff58374176491155738459cd03d0cde6
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 22:38:24 +0000

    Pushing changes with require

--- head.js
+++ head.js
@@ -1,9 +1,7 @@
-const assertEqual = require('./assertEqual')
+const assertEqual = require('./assertEqual');
 
 const head = function(thisArray) {
-  return thisArray[0]
+  return thisArray[0];
 }
-assertEqual(head([5,6,7]),5)
-assertEqual(head(["Basketball", "Steak", "Lighthouse"]), "Labs");
 
-module.exports = head.js
+module.exports = head;
