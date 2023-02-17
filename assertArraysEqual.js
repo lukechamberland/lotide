19cdd9c1f44566aad7a513c7420c781ab2b5e685
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 07:21:46 +0000

    Pushing Changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,27 @@
-const assertEqual = function(actual, expected) {
-  if (actual.length == expected.length && Array.isArray(actual) === Array.isArray(expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
+      return false;
+    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
+      eqArrays(first[i], second[i]);
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqArrays2 = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 }
 
-assertEqual(['hello'], ['hello'])
+module.exports = assertArraysEqual
