4bd60cb6203ef95b26c0cfca70f6f70c16b3a82c
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-17 22:32:46 +0000

    Pushing changes with Mocha and Chai

--- test/testMiddle.js
+++ test/testMiddle.js
@@ -3,9 +3,9 @@ const middle = require('../middle');
 
 describe("#middle", () => {
   it("returns 2 for [1, 2, 3]", () => {
-    assertArraysEqual(middle([1, 2, 3]), 2);
+    assertArraysEqual(middle([1, 2, 3]), [2]);
   });
   it("returns 3 for [1, 2, 3, 4, 5]", () => {
-    assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
+    assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 });
