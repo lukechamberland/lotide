5cbe353692a413bc0fbc287e1bf7ad655cc41372
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 22:24:16 +0000

    Added necassary comments and to describe how the function was working

--- letterPositions.js
+++ letterPositions.js
@@ -1,38 +1,14 @@
-const eqArrays = function(first, second) {
-  if (first.length !== second.length) {
-    return false;
-  }
-  for (let i = 0; i < first.length; i++) {
-    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
-      return false;
-    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
-      eqArrays(first[i], second[i]);
-    } else if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const eqArrays2 = require("./eqArrays");
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-assertArraysEqual(['hello'], ['hello!']);
-
 const letterPositions = function(finalSentance) {
+  //changes to an array
   let newSentance = finalSentance.split('');
   let results = {};
+  // returns the position of each occurance of the letter as an array
   newSentance.forEach(function(letter, i) {
     if (letter !== " ") {
       results[letter] ? results[letter].push(i) : results[letter] = [i];
     }
   });
+  // returns the final results
   return results;
 };
 
