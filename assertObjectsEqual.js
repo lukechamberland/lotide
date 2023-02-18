c332c06947717d059ecc23f72410e18635e1c65c
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 01:03:32 +0000

    Pushing Changes to GitHub

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,14 +1,5 @@
 const eqObjects = require("./eqObjects");
 
-const assertObjectEqual = function(object1, object2) {
-  const inspect = require('util').inspect;
-  if (eqObjects(object1, object2)) {
-    console.log(`Assertion passed: ${inspect(object1)} === ${inspect(object2)}`);
-  } else {
-    console.log(`Assertion failed: ${inspect(object1)} !== ${inspect(object2)}`);
-  }
-};
-
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
