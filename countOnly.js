e9f800ace16916d090dd4f60c9d7311abe260112
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 21:25:00 +0000

    used require to import assert equal instead of copy and pasting the function from its own file

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+const assertEqual = require('./assertEqual')
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
@@ -21,16 +15,5 @@ const countOnly = function(allItems, itemsToCount) {
     return results;
   }
 }
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
 
 module.exports = countOnly;
