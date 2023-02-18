f2cbc8e54329c7b596b3ef63c2f975b7224b29a1
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 22:50:11 +0000

    added comments and removed unneeded functions

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,9 @@
 const eqArrays = function(first, second) {
+  // if argument lengths are not equal returns false
   if (first.length !== second.length) {
     return false;
   }
+  // loop campares if first and second are both arrays and if each element of the array is equal
   for (let i = 0; i < first.length; i++) {
     if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
       return false;
