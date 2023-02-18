d30c0de502eac69e8e64f9fa396e6efe010d4140
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 01:12:01 +0000

    Pushing Changes to GitHub

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,5 @@
 const takeUntil = function(array, callback) {
-  let newArray = [];
+  newArray = [];
   for (let element of array) {
     if (callback(element) === false) {
        newArray.push(element);
@@ -10,6 +10,4 @@ const takeUntil = function(array, callback) {
   return newArray;
 }
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+module.exports = takeUntil;
