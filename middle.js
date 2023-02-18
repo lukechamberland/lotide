c45ab0f05e8456d23690e388870a7929e6b287d6
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 22:16:12 +0000

    added neccasarry comments to describe the if statement and what it was returning

--- middle.js
+++ middle.js
@@ -1,12 +1,11 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
 const middle = function(array) {
-
+  // makes sure the array length is at least 3
   if (array.length <= 2) {
     return [];
-  } else if (array.length % 2 === 0) {
+    // if the array length is even returns the middle 2 numbers
+  } else if (array.length % 2 === 0) { 
     return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  // if array length is odd return the one middle number
   } else {
     return [array[Math.floor(array.length / 2)]];
   }
