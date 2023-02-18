de153dd3176c2eede7adf032028bfce44239f013
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 21:33:01 +0000

    the copy and pasted function assertArraysEqual and eqArrays and imported them from their own files

--- flatten.js
+++ flatten.js
@@ -1,28 +1,5 @@
-const eqArrays = function(first, second) {
-  if (first.length !== second.length) {
-    return false;
-  }
-  for (let i = 0; i < first.length; i++) {
-    if ((Array.isArray(first[i]) || Array.isArray(second[i])) && eqArrays(first[i], second[i]) === false) {
-      return false;
-    } else if (Array.isArray(first[i]) || Array.isArray(second[i])) {
-      eqArrays(first[i], second[i]);
-    } else if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const eqArrays2 = require("./eqArrays");
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const flatten = function(array) {
   let secondArray = [];
