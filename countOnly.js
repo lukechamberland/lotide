99d6f6ffef5f028bf558d51918b9d73c4432f77e
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 01:04:22 +0000

    Pushing Changes to GitHub

--- countOnly.js
+++ countOnly.js
@@ -33,9 +33,4 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined); 
+module.exports = countOnly
