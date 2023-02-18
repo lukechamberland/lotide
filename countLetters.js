fec74baf91b98ec9200b27af2be2be778dc7174a
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 22:59:52 +0000

    added comments describing the loop and operators

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,13 @@
-const assertEqual = require('./assertEqual');
-
 const countLetters = function(string) {
+  // removes spaces
   let newString = string.split(" ").join('');
+  //storing the final result
   let result = {};
+  //looping through newString and checking if result is truthy or falsey
   for (let element of newString) {
     result[element] ? result[element] += 1 : result[element] = 1;
   }
+  // returning the final result
   return result;
 };
 
