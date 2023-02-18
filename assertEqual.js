7faf62fce3fb13c3132b7794fac62c9564e439fe
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 23:59:58 +0000

    removed unneeded comment and structured code properly

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,9 @@
 const assertEqual = function(actual, expected) {
-  // simply checks if two arguments are equal
   if (actual === expected) {
-    // interpolates them if they are equal
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    //interpolates them if they are not equal
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 }
-
 
 module.exports = assertEqual;
