184960d374e4ed8c47dcc119fad4bcdf8b7f9fbb
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 23:27:52 +0000

    changed comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,8 @@
 const eqArrays = require('./eqArrays');
-// checks if arrays are equal or not
+
 const assertArraysEqual = function(actual, expected) {
-  // interpolates if equal
   if (eqArrays(actual, expected)) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
-  // interpolates if not equal
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
