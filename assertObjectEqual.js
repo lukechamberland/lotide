457234e9edc5ee0a8463a44d9d2e0c847f018907
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 23:13:13 +0000

    added a single comment to explain string interpolation

---
+++ assertObjectEqual.js
@@ -0,0 +1,14 @@
+const eqObjects = require("./eqObjects");
+
+const assertObjectEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  // uses eqObjects function to check if they are equal or not, and uses string interpolation to log final output to the console
+  if (eqObjects(object1, object2)) {
+    console.log(`Assertion passed: ${inspect(object1)} === ${inspect(object2)}`);
+  } else {
+    console.log(`Assertion failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+
+module.exports = assertObjectEqual;
+
