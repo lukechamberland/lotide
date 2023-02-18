1bb696d19cffcc05e7a258f1d8d999fc85c86051
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 23:19:24 +0000

    added comments describing the if statements and string interpolation

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,10 @@
 const eqArrays = require('./eqArrays');
-
+// checks if arrays are equal or not
 const assertArraysEqual = function(actual, expected) {
+  // interpolates if equal
   if (eqArrays(actual, expected)) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
+  // interpolates if not equal
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
