cc5bafa58916b0ee5db096f9d52e124925e35e9c
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 22:30:04 +0000

    added comments to describe the if statements

--- flatten.js
+++ flatten.js
@@ -1,14 +1,15 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
 const flatten = function(array) {
+  // final results
   let secondArray = [];
+  // loops through the given array
   array.forEach(function(firstElement) {
+    // if the element is an array push each element to secondArray
     if (Array.isArray(firstElement)) {
       firstElement.forEach(function(secondElement) {
         secondArray.push(secondElement);
       });
     } else {
+      // if not an array push the element to secondArray
       secondArray.push(firstElement);
     }
   });
