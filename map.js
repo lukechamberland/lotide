9015aecacbecf0e2294bb5230ea01e946355e990
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 01:07:02 +0000

    Pushing Changes to GitHub

--- map.js
+++ map.js
@@ -1,18 +1,8 @@
 const map = function(array, callback) {
-  const results = []
+  const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
-  return results
+  return results;
 }
-const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+module.exports = map
