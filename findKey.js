9469d369b7ed067bb960480587fbcef1ef3cba8f
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-18 01:05:25 +0000

    Pushing Changes to GitHub

--- findKey.js
+++ findKey.js
@@ -1,10 +1,3 @@
 const findKey = (object, callback) => Object.keys(object).find(key => callback(object[key]));
 
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2))
+module.exports = findKey
