89dca85f64ebf8d47277d22b7d0974e95bad161b
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:04:48 +0000

    removed useless comments and structured code in a more organized way

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,11 @@
 const countLetters = function(string) {
   // removes spaces
   let newString = string.split(" ").join('');
-  //storing the final result
   let result = {};
   //looping through newString and checking if result is truthy or falsey
   for (let element of newString) {
     result[element] ? result[element] += 1 : result[element] = 1;
   }
-  // returning the final result
   return result;
 };
 
