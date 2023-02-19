da5b5e6cb9cbb2114940821431d486ebb63f9756
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:23:50 +0000

    removed an unnecassary comment

--- takeUntil.js
+++ takeUntil.js
@@ -1,15 +1,12 @@
 const takeUntil = function(array, callback) {
   newArray = [];
-  // loop through array
   for (let element of array) {
-    // push false arguments to newArray
     if (callback(element) === false) {
        newArray.push(element);
     } else {
       return newArray;
     }
   }
-  // return the new array
   return newArray;
 }
 
