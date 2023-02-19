d0ec4387916a441407c672030953eacd4a2a2967
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:20:51 +0000

    removed a comment that I didn't need and added a necassary line

--- map.js
+++ map.js
@@ -1,10 +1,9 @@
 const map = function(array, callback) {
-  // final results stores in results
-  const results = [];
-  // looping through the array 
+  const results = []; 
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
 }
+
 module.exports = map;
