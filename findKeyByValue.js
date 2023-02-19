f963940d4e0648717dea033a0aae1bf9a71f51b2
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:12:30 +0000

    removed a comment that wasn't needed

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,7 +1,5 @@
 const findKeyByValue = function(object, value) {
-  // loops through the obejct
   for (let key in object) {
-    // returns the first key which is equal to the given value
     if (value === object[key]) {
       return key;
     }
