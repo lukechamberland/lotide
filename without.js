027a4f42405e2714f8b2109f33f48e1d973259df
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:29:22 +0000

    reworded a necassary comment

--- without.js
+++ without.js
@@ -1,9 +1,7 @@
 const without = function(items, itemsToRemove) {
   const array = [];
   for (const item of items) {
-    /* if itemsToRemove doesn't include 
-    the given item from the loop, push it 
-    to array */
+    // if itemsToRemove doesn't include the given item from the loop, push it to array 
     if (!itemsToRemove.includes(item)) {
       array.push(item);
     }
