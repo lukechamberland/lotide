84a4eba8676fe160b1e8ace4845f28e099d30a84
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-19 00:06:13 +0000

    removed unnecassary comments

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,5 @@
 const countOnly = function(allItems, itemsToCount) {
-  // new object that will contain count that the given object listed
   const results = {};
-  // loops through allItems
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
